f43843a0a5ac5f0e4e0c899555fbde986505990b
Zahid Wazifa <acerzifa@gmail.com>
2025-02-08 17:54:42 +0700

    feat: adding src and gitignore

---
+++ src/vuln.c
@@ -0,0 +1,27 @@
+
+/*
+ * phoenix/stack-five, 
+ *
+ * Can you execve("/bin/sh", ...) ?
+ *
+ * What is green and goes to summer camp? A brussel scout.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BANNER "Welcome to Chapter5, brought to you by Something"
+
+char *gets(char *);
+
+void start_level() {
+  char buffer[128];
+  gets(buffer);
+}
+
+int main(int argc, char **argv) {
+  printf("%s\n", BANNER);
+  start_level();
+}
